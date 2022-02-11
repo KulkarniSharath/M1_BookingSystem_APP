cbc7e7ee6a646ac57aba14347eb4bfce0733af27
KulkarniSharath <98825618+KulkarniSharath@users.noreply.github.com>
2022-02-11 17:32:45 +0530

    Add files via upload

---
+++ 3_Implementation/code.h
@@ -0,0 +1,58 @@
+#include "code.h"
+
+// Header gaurd, mandatory
+#ifndef CODE_H_
+#define CODE_H_ 
+
+
+
+/**
+ * @brief calculatins
+ * 
+ */
+
+enum status int cal(int y1, int y2, int h)
+
+/**
+ * @brief Adding passenger details
+ * 
+ */
+enum status void add_node(char lol[20], char der[6], int b)
+
+/**
+ * @brief Passenger inputs
+ * 
+ */
+enum status void details(int k)
+
+/**
+ * @brief Seat matrix selection
+ * 
+ */
+
+enum status int seat(int p)
+
+/**
+ * @brief Printing recipt
+ * 
+ */
+
+enum status void bill(int y, int j)
+
+/**
+ * @brief Driver Initialization
+ * 
+ */
+
+enum status void main()
+
+
+# endif
+
+
+
+
+
+
+
+
